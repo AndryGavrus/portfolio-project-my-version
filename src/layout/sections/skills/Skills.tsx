be3586635686f49001ce2e287b6751a465fb74ee
Andrey <andry.gavrus@gmail.com>
2024-10-13 19:23:45 +0300

    Add SillsScrolBar

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,13 +3,45 @@ import styled from 'styled-components'
 import { SectionTitle } from '../../../assets/components/SectionTitle'
 import { LangBlock } from './blocks/LangBlock'
 import { StudiesBlock } from './blocks/StudiesBlock'
-import { SkillsBar } from './skillsBar/SkillsBar'
+import { Skill } from './skillsBar/skill/Skill'
+
+
+const SkillData = [
+    {
+        iconId: 'photoshopPB',
+        title: 'Adobe PhotoShop',
+        mark: '4.0'
+    },
+
+    {
+        iconId: 'excelPB',
+        title: 'Microsoft Excel',
+        mark: '4.8'
+    },
+
+    {
+        iconId: 'photoshopPB',
+        title: 'Adobe PhotoShop',
+        mark: '4.0'
+    },
+
+    {
+        iconId: 'excelPB',
+        title: 'Microsoft Excel',
+        mark: '4.8'
+    },
+]
 
 export const Skills = () => {
     return (
         <SkillsSection>
             <SectionTitle>My Skills</SectionTitle>
-            <SkillsBar/>
+            <SkillsBar>
+            {SkillData.map((s, index) => {
+                return (<Skill key={index} iconId={s.iconId} title={s.title} mark={s.mark} />
+                )
+            })}
+            </SkillsBar>
             <LangBlock/>
             <StudiesBlock/>
         </SkillsSection>
@@ -19,4 +51,10 @@ export const Skills = () => {
 const SkillsSection = styled.section`
     background-color: aqua;
     min-height: 100vh;
+`
+
+const SkillsBar = styled.div`
+    display: flex;
+    justify-content: space-around;
+    flex-grow: 1;
 `
---
+++ src/layout/sections/skills/skillsBar/skill/Skill.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Icon } from '../../../../../assets/components/icon/Icon'
+import { FlexWrapper } from '../../../../../assets/components/FlexWrapper'
+
+
+type SkillPropsType = {
+    iconId: string
+    title: string
+    mark: string
+}
+
+export const Skill = (props: SkillPropsType) => {
+    return (
+        <StyledSkill>
+            <FlexWrapper justify='center' align='center'>
+                <Icon iconId={props.iconId} width='280' height='280' viewBox='0 0 280 280'/>
+                <Mark>{props.mark}</Mark>
+            </FlexWrapper>
+            <SkillName>{props.title}</SkillName>
+        </StyledSkill>)
+}
+
+const StyledSkill = styled.div`
+
+`
+
+const Mark = styled.span`
+    display: flex;
+    position: absolute;
+`
+
+
+const SkillName = styled.h3`
+    text-align: center;
+`
